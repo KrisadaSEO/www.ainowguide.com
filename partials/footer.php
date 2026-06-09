ab7740f4789f944f473deeb95f2d165239613ca4
KrisadaSEO <krisadaseo@gmail.com>
2026-06-09 19:43:05 -0400

    Add Statcounter analytics code to footer

--- partials/footer.php
+++ partials/footer.php
@@ -42,5 +42,23 @@ $columns  = $footer['columns'] ?? [];
 
 <script src="/assets/js/main.js"></script>
 </div><!-- /.site-wrapper -->
+
+<!-- Default Statcounter code for ! - *AINowGuide.com
+https://www.ainowguide.com/ -->
+<script type="text/javascript">
+var sc_project=12961705;
+var sc_invisible=1;
+var sc_security="3648782d";
+var sc_remove_link=1;
+</script>
+<script type="text/javascript"
+src="https://www.statcounter.com/counter/counter.js"
+async></script>
+<noscript><div class="statcounter"><img class="statcounter"
+src="https://c.statcounter.com/12961705/0/3648782d/1/"
+alt="Web Analytics Made Easy - Statcounter"
+referrerPolicy="no-referrer-when-downgrade"></div></noscript>
+<!-- End of Statcounter Code -->
+
 </body>
 </html>
